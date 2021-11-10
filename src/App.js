4fc6c3040703ebe6d352efb21925a6d2559668a3
Marta Vaclavikova <marta.vaclavikova@seznam.cz>
2021-11-10 09:10:57 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -11,44 +11,37 @@ const App = () => {
   const [forecast, setForecast] = useState(null);
   const [city, setCity] = useState('Prague');
 
-  const fetchWeather = () => {
+
+  useEffect(() => {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_MY_API_ID}`)
       .then(response => response.json())
       .then(json => {
         setWeather(json);
-      })
-  }
+      });
 
-  const fetchForecast = () => {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${process.env.REACT_APP_MY_API_ID}`)
       .then(response => response.json())
       .then(json => {
         setForecast(json.list.filter((_, index) => index % 8 === 0));
-        console.log(forecast);
-      })
-  }
-
-  useEffect(() => {
-    fetchWeather();
-    fetchForecast();
+      });
   }, [city]);
 
   return (
     <div className="App">
       <div className="container">
         <h1>My Weather App</h1>
+        <div className="select-wrapper">
+          <select
+            className="select"
+            name="cityselect"
+            id="cityselect"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          >
+            {cities.map(city => <option value={city} key={city}>{city}</option>)}
+          </select>
+        </div>
         <div className="weather">
-          <div className="select-wrapper">
-            <select
-              className="select"
-              name="cityselect"
-              id="cityselect"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            >
-              {cities.map(city => <option value={city} key={city}>{city}</option>)}
-            </select>
-          </div>
           {(weather !== null && weather !== undefined) ? <Weather weather={weather} /> : <Loading />}
           {(forecast !== null && forecast !== undefined) ? <Forecast forecast={forecast} /> : <Loading />}
         </div>
